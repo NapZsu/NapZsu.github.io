43c91960ba4f4b9f81cf9214c30cfee5a70a167b
NapZsu <palkonzs@gmail.com>
2021-07-24 20:31:10 +0200

    kevesebbKep

--- masodik/scripts.js
+++ masodik/scripts.js
@@ -59,60 +59,8 @@ let cim9 = {
     KepLeiras: 'Szilveszter délután az erdőn...'
   };
 
-let cim11 = {
-    foto: 'images/11.JPG',
-    KepCim: 'Téli tuskó',
-    KepLeiras: 'Gomba, moha és zuzmó...'
-  };
 
-let cim12 = {
-    foto: 'images/12.JPG',
-    KepCim: 'Kilátó',
-    KepLeiras: 'Mit is látunk?...'
-  };
- 
-  let cim13 = {
-    foto: 'images/13.JPG',
-    KepCim: 'Akasztódomb',
-    KepLeiras: 'Innét még pont haza látok...'
-  };
-
-let cim14 = {
-    foto: 'images/14.JPG',
-    KepCim: 'Jégbogyók',
-    KepLeiras: 'Balatonpart...'
-  };
-
-let cim15 = {
-    foto: 'images/15.JPG',
-    KepCim: 'Jégvilág',
-    KepLeiras: 'Balaton télen...'
-  };
-
-  let cim16 = {
-    foto: 'images/16.JPG',
-    KepCim: 'Hársas-tó',
-    KepLeiras: 'Őszi kirándulás...'
-  };
-
-let cim17 = {
-    foto: 'images/17.JPG',
-    KepCim: 'Kis-Balaton',
-    KepLeiras: 'Csendes nyugalom...'
-  };
-
-let cim18 = {
-    foto: 'images/18.JPG',
-    KepCim: 'Ebédszünet a Kányavári szigeten',
-    KepLeiras: 'Kis-Balaton...'
-  };
-  let cim19 = {
-    foto: 'images/19.JPG',
-    KepCim: 'Kis-Balaton',
-    KepLeiras: 'Fent az ég és lent a víz...'
-  };
-
-let cimek=[cim1, cim2, cim3, cim4, cim5, cim6, cim7, cim8, cim9, cim10, cim11, cim12, cim13, cim14, cim15, cim16, cim17, cim18, cim19]
+let cimek=[cim1, cim2, cim3, cim4, cim5, cim6, cim7, cim8, cim9, cim10]
 let min=0
 let max=cimek.length-1
 $("#foto").attr('src', cimek[aktualisKep].foto);
